1c7df75cb5fe3c920860a937ec4ad382d4540b71
Ayush Singhal <ayush.singhal@joshtechnologygroup.com>
2021-02-08 20:15:12 +0530

    fixed cors headers

--- features/routes/auth/auth.go
+++ features/routes/auth/auth.go
@@ -2,6 +2,7 @@ package auth
 
 import (
 	"code.jtg.tools/ayush.singhal/notifications-microservice/features/controllers/auth"
+	"code.jtg.tools/ayush.singhal/notifications-microservice/features/routes/preflight"
 	"github.com/gin-gonic/gin"
 )
 
@@ -9,4 +10,6 @@ import (
 func Auth(router *gin.RouterGroup) {
 	router.GET("/token/:token", auth.ValidateEmail)
 	router.POST("/token", auth.RefreshAccessToken)
+	router.OPTIONS("/token/:token", preflight.Preflight)
+	router.OPTIONS("/token", preflight.Preflight)
 }
