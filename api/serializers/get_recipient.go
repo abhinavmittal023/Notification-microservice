71dba37c714cf58f24c713e0ada274677fb9a748
Ayush Singhal <ayush.singhal@joshtechnologygroup.com>
2021-02-15 16:08:17 +0530

    added get_recipient serializer

---
+++ api/serializers/get_recipient.go
@@ -0,0 +1,23 @@
+package serializers
+
+import (
+	"code.jtg.tools/ayush.singhal/notifications-microservice/db/models"
+)
+
+// RecipientInfo serializer to bind request data
+type RecipientInfo struct {
+	ID                 uint   `json:"recipient_id"`
+	Email              string `json:"email,omitempty"`
+	PushToken          string `json:"push_token,omitempty"`
+	WebToken           string `json:"web_token,omitempty"`
+	PreferredChannelID uint64 `json:"preferred_channel_id,omitempty"`
+}
+
+// RecipientModelToRecipientInfo converts the Recipient model to RecipientInfo struct
+func RecipientModelToRecipientInfo(info *RecipientInfo, recipient *models.Recipient) {
+	info.ID = recipient.ID
+	info.Email = recipient.Email
+	info.PushToken = recipient.PushToken
+	info.WebToken = recipient.WebToken
+	info.PreferredChannelID = recipient.PreferredChannelID
+}
