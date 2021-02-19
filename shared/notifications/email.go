0a18034da2f69bd9053a84aea388df2faafa6292
Ayush Singhal <ayush.singhal@joshtechnologygroup.com>
2021-02-19 18:15:07 +0530

    Now using MailCatcher instead of gmail

--- shared/notifications/email.go
+++ shared/notifications/email.go
@@ -36,7 +36,7 @@ func (email *Email) SendNotification() error {
 	//  Sending email.
 	err := smtp.SendMail(addr, auth, from, email.to, msg)
 	if err != nil {
-		log.Println("Unable to send email")
+		log.Println("Hello")
 		return errors.Wrap(err, "Unable to send email")
 	}
 	log.Println("Email Sent Successfully!")
