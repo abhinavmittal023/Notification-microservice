0fd048572721d442a27d368cdd10c4307963fb61
Ayush Singhal <ayush.singhal@joshtechnologygroup.com>
2021-02-22 15:24:31 +0530

    added web notification function

---
+++ shared/notifications/web.go
@@ -0,0 +1,49 @@
+package notifications
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"code.jtg.tools/ayush.singhal/notifications-microservice/configuration"
+)
+
+// PushNotification function sends a push notification to the specified deviceToken given the server key and title, body of the notification
+func PushNotification(deviceToken string, serverKey string, title string, notificationBody string) (bool, error) {
+	url := configuration.GetResp().PushNotification.URL
+
+	var jsonStr = []byte(fmt.Sprintf(`{"notification": {
+		"title": "%s", 
+		"body": "%s"
+		},
+		"to" : "%s"}`, title, notificationBody, deviceToken))
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+	req.Header.Set("Authorization", fmt.Sprintf("key=%s", serverKey))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return false, err
+	}
+	defer resp.Body.Close()
+
+	// Declared an empty map interface
+	var result map[string]interface{}
+
+	if resp.Status != "200 OK" {
+		return false, fmt.Errorf("Non 200 status received")
+	}
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	// Unmarshal or Decode the JSON to the interface.
+	json.Unmarshal(body, &result)
+
+	if result["success"] != 1.0 {
+		return false, nil
+	}
+	return true, nil
+}
